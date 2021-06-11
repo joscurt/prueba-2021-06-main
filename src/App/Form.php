35cc765fb204da8796353e41ec99ae9ba6eea5b9
Jose <joscurt@gmail.com>
2021-06-10 23:00:31 -0400

    Creando el formulario

--- src/App/Form.php
+++ src/App/Form.php
@@ -10,6 +10,16 @@ class Form
     public static function generate(): string
     {
         // añade tu código aquí
-        return 'hola';
+        $dataform = file_get_contents("./schema.json");
+
+        if( !is_array($dataform) ) {
+			       $dataform = json_decode( $dataform, true );
+        }
+        // var_dump($dataform);exit;
+
+        $formulario = '<form>';
+        $formulario .= '</form>';
+
+        return $formulario;
     }
 }

1fb6ad4acf6d35d40649850b3b8e49a9fd90370c
Jose <joscurt@gmail.com>
2021-06-11 01:39:40 -0400

    Creación de los otros input date, height, number

--- src/App/Form.php
+++ src/App/Form.php
@@ -4,13 +4,9 @@ namespace App;
 
 class Form
 {
-
     /**
      * Clase generadora de formulario
      */
-     function __construct() {
-
-	    }
     public static function generate(): string
     {
         // añade tu código aquí
@@ -19,18 +15,20 @@ class Form
         if( !is_array($dataform) ) {
 			       $dataform = json_decode( $dataform, true );
         }
-        // var_dump($dataform);exit;
+        // var_dump($dataform["properties"]["personalData"]);exit;
 
-        $formulario = '<form>';
+        $formulario = '<div class="container"><div class="row"><form>';
 
         // Recorro el array que contiene los elementos q se crean en el formulario
         // dejo la variable $key pq el esquema los usa como label
+        $myForm = new Form();
         foreach($dataform["properties"] as $key => $datainput ){
-          $myForm = new Form();
           $formulario .= $myForm->crearInput($datainput, $key);
         }
-
-        $formulario .= '</form>';
+        foreach($dataform["properties"]["personalData"]["properties"] as $key => $datainput){
+          $formulario .= $myForm->crearInput($datainput, $key);
+        }
+        $formulario .= '</form></div></div>';
 
         return $formulario;
     }
@@ -63,23 +61,43 @@ class Form
   				$cinput .= '<option value="'.$option.'">'.$option.'</option>'.PHP_EOL;
   			}
 
-  			$cinput .= '</select><br></div>'.PHP_EOL;
+  			$cinput .= '</select><br></div></div>'.PHP_EOL;
 
   		}
-
   		elseif (isset($datainput["format"])){
-  			$cinput .= '<div class="select is-primary">'.PHP_EOL;
+  			// $cinput .= '<div class="select is-primary">'.PHP_EOL;
   			$cinputparametros = "type=date".PHP_EOL;
   			$cinputparametros .= "class=form-control".PHP_EOL;
   			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
-  			$cinput .= '<input '.$cinputparametros.'/>'.PHP_EOL;
-  			$cinput .= '</select><br></div>'.PHP_EOL;
+  			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
   		}
-  		 else {
+      elseif (($datainput["type"])=="boolean") {
+        // $cinput .= '<div class="select is-primary">'.PHP_EOL;
+  			$cinputparametros = "type=checkbox".PHP_EOL;
+  			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
+  			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
+      }
+      elseif ($datainput["type"]=="integer") {
+          $cinputparametros .= "type=number".PHP_EOL;
+          $cinputparametros .= "min=0".PHP_EOL;
+          $cinputparametros .= "data-bind=value:replyNumber".PHP_EOL;
+          $cinputparametros .= "id=replyNumber".PHP_EOL;
+    			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
+    			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
+      }
+      elseif ($datainput["type"]=="number") {
+          $cinputparametros .= "type=number".PHP_EOL;
+          $cinputparametros .= "step=0.1".PHP_EOL;
+          $cinputparametros .= "data-bind=value:replyNumber".PHP_EOL;
+          $cinputparametros .= "id=replyNumber".PHP_EOL;
+    			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
+    			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
+      }
+  		else {
   			//Si no es un select creo un input clásico
   			$cinputparametros .= "class=input".PHP_EOL;
   			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
-  			$cinput .= '<input '.$cinputparametros.'/>'.PHP_EOL;
+  			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
   		}
       return $cinput;
     }
@@ -88,7 +106,7 @@ class Form
       if(is_array($datalabel)){
         $datalabel = json_decode($datalabel,true);
       }
-      $datalabel = '<label '.$datalabel.'>'.$datalabel.'</label>'.PHP_EOL;
+      $datalabel = '<div class="col"><label '.$datalabel.'>'.$datalabel.'</label>'.PHP_EOL;
       return $datalabel;
     }
 }

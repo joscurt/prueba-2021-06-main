482a92634f32c212bbede6e59b6025d438e2a3b3
Jose <joscurt@gmail.com>
2021-06-11 12:57:05 -0400

    Ordenando un poco el código

--- src/App/Form.php
+++ src/App/Form.php
@@ -10,12 +10,14 @@ class Form
     public static function generate(): string
     {
         // añade tu código aquí
+
+
         $dataform = file_get_contents("./schema.json");
 
+        //formateo del json
         if( !is_array($dataform) ) {
 			       $dataform = json_decode( $dataform, true );
         }
-        // var_dump($dataform["properties"]["personalData"]);exit;
 
         $formulario = '<div class="container"><div class="row"><form>';
 
@@ -35,7 +37,7 @@ class Form
 
     function crearInput($datainput, $key){
 
-      if( !is_array($datainput) ) {
+        if( !is_array($datainput) ) {
 			     $datainput = json_decode( $datainput, true );
         }
         $cinput = '';
@@ -51,56 +53,50 @@ class Form
 
         // Creo el Select del schema, consulto si existe la etiqueta "enum" y luego recorro para listar los values
         if(isset($datainput["enum"])) {
-        // $cinputparametros = $datainput["enum"];
-  			$cinput .= '<div class="select is-primary">'.PHP_EOL;
-  			// $inputparams .= 'class="select is-primary"'.PHP_EOL;
-  			$cinput .= '<select '.$cinputparametros.' />'.PHP_EOL;
+    			$cinput .= '<div class="select is-primary">'.PHP_EOL;
+    			$cinput .= '<select '.$cinputparametros.' />'.PHP_EOL;
 
   			foreach( $datainput["enum"] as $option ) {
-  				// var_dump($option);exit;
   				$cinput .= '<option value="'.$option.'">'.$option.'</option>'.PHP_EOL;
   			}
-
   			$cinput .= '</select><br></div></div>'.PHP_EOL;
-
-  		}
-  		elseif (isset($datainput["format"])){
-  			// $cinput .= '<div class="select is-primary">'.PHP_EOL;
-  			$cinputparametros = "type=date".PHP_EOL;
-  			$cinputparametros .= "class=form-control".PHP_EOL;
-  			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
-  			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
-  		}
-      elseif (($datainput["type"])=="boolean") {
-        // $cinput .= '<div class="select is-primary">'.PHP_EOL;
-  			$cinputparametros = "type=checkbox".PHP_EOL;
-  			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
-  			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
-      }
-      elseif ($datainput["type"]=="integer") {
+  		  }
+  		  elseif (isset($datainput["format"])){
+    			$cinputparametros = "type=date".PHP_EOL;
+    			$cinputparametros .= "class=form-control".PHP_EOL;
+    			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
+    			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
+  		  }
+        elseif (($datainput["type"])=="boolean") {
+    			$cinputparametros = "type=checkbox".PHP_EOL;
+    			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
+    			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
+        }
+        elseif ($datainput["type"]=="integer") {
           $cinputparametros .= "type=number".PHP_EOL;
           $cinputparametros .= "min=0".PHP_EOL;
           $cinputparametros .= "data-bind=value:replyNumber".PHP_EOL;
           $cinputparametros .= "id=replyNumber".PHP_EOL;
-    			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
-    			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
-      }
-      elseif ($datainput["type"]=="number") {
+      		$cinputparametros .= 'placeholder='.$key.PHP_EOL;
+      		$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
+        }
+        elseif ($datainput["type"]=="number") {
           $cinputparametros .= "type=number".PHP_EOL;
           $cinputparametros .= "step=0.1".PHP_EOL;
           $cinputparametros .= "data-bind=value:replyNumber".PHP_EOL;
           $cinputparametros .= "id=replyNumber".PHP_EOL;
-    			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
-    			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
-      }
+      		$cinputparametros .= 'placeholder='.$key.PHP_EOL;
+      		$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
+        }
   		else {
   			//Si no es un select creo un input clásico
   			$cinputparametros .= "class=input".PHP_EOL;
   			$cinputparametros .= 'placeholder='.$key.PHP_EOL;
   			$cinput .= '<input '.$cinputparametros.'/></div>'.PHP_EOL;
   		}
-      return $cinput;
+    return $cinput;
     }
+
     function crearLabel($datalabel){
 
       if(is_array($datalabel)){
